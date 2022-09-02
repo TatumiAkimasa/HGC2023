ef86391c3d1dc807bf762ef73556e7ff75eeb435
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2022-09-02 15:14:08 +0900

    調整

--- Nechronica/Assets/tatumi/Script/Script/MOve_chara.cs
+++ Nechronica/Assets/tatumi/Script/Script/MOve_chara.cs
@@ -1,49 +1,51 @@
-//include = using
-using UnityEngine;
-using UnityEngine.UI;
 
-//○○:継承名
-//また、クラス名とファイル名があってないとエラーが出る。
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class MOve_chara : MonoBehaviour
 {
-    public float speed,Pjump; // 動く速さ
+    public float speed = 10.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 320.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
   
-    private Rigidbody rb; // Rididbody
-   
-    //初期化
+    private CharacterController controller;
+
+    private float my_y;
+
+
+
     void Start()
     {
-        // Rigidbody を取得
-        rb = GetComponent<Rigidbody>();
-
-       
+        controller = GetComponent<CharacterController>();
     }
 
-    //アクション（毎回実行）
     void Update()
     {
-        // カーソルキーの入力を取得
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-
-        //var=変数(型は特になし)という意
-
-        // カーソルキーの入力に合わせて移動方向を設定
-        var movement = new Vector3(moveHorizontal, 0, moveVertical);
-
-        // Ridigbody に力を与えて玉を動かす（剛体）
-        rb.AddForce(movement * speed);
-
-        if(Input.GetKey(KeyCode.Space))
+        if (controller.isGrounded)
         {
-            // カーソルキーの入力に合わせて移動方向を設定
-            var jump = new Vector3(0, 1, 0);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
 
-            // Ridigbody に力を与えて玉を動かす（剛体）
-            rb.AddForce(jump * Pjump);
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, my_y, Input.GetAxis("Vertical") * speed);
+            
         }
 
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        my_y = moveDirection.y;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 
-   
+  
 }
