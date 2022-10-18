3b3d9550a28e3b70698d12582b89715921fb97b2
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2022-10-18 14:24:33 +0900

    no message

--- Nechronica/Assets/tatumi/Script/Script/UI_Chara_status.cs
+++ Nechronica/Assets/tatumi/Script/Script/UI_Chara_status.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Doll_Data : MonoBehaviour
+public class UI_Chara_status : MonoBehaviour
 {
-  
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
