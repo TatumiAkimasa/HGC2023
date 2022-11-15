d2fb7e6cc7d5d6dab248a753e7ffb06521be1a5d
bontellis <72423564+bontellis@users.noreply.github.com>
2022-11-15 13:25:37 +0900

    追加されていたasettデータ

---
+++ Nechronica/Assets/tatumi/Script/Input_data_Button/Input_data_Button.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Input_data_Button : ClassData_
+{
+    
+
+    [SerializeField]
+    private Text input_text,output_text;
+  
+    private ClassData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Push_button()
+    {
+        output_text.text = input_text.text;
+    }
+
+    public string Push_button_pos()
+    {
+       
+        output_text.text = input_text.text;
+
+        if (input_text.text == "アリス")
+            return "Alis";
+        else if (input_text.text == "ホリック")
+            return "Holic";
+        else if (input_text.text == "オートマトン")
+            return "automaton";
+        else if (input_text.text == "コート")
+            return "coat";
+        else if (input_text.text == "ジャンク")
+            return "junk";
+        else if (input_text.text == "ソロリティ")
+            return "Sorority";
+        else
+            return "error";
+;
+    }
+
+    public ClassData Push_button_class()
+    {
+        data.parts = new int[3];
+        output_text.text = input_text.text;
+
+        //ClassData data;
+
+        if (input_text.text == "ステーシー")
+        {
+            data.parts[ARMAMENT] = 1;
+            data.parts[VARIANT] = 1;
+            data.parts[ALTER] = 0;
+            
+        }
+        else if (input_text.text == "タナトス")
+        {
+            data.parts[ARMAMENT] = 1;
+            data.parts[VARIANT] = 0;
+            data.parts[ALTER] = 1;
+            
+        }
+        else if (input_text.text == "ゴシック")
+        {
+            data.parts[ARMAMENT] = 0;
+            data.parts[VARIANT] = 1;
+            data.parts[ALTER] = 1;
+           
+        }
+        else if (input_text.text == "レクイエム")
+        {
+            data.parts[ARMAMENT] = 2;
+            data.parts[VARIANT] = 0;
+            data.parts[ALTER] = 0;
+            
+        }
+        else if (input_text.text == "バロック")
+        {
+            data.parts[ARMAMENT] = 0;
+            data.parts[VARIANT] = 2;
+            data.parts[ALTER] = 0;
+          
+        }
+        else if (input_text.text == "ロマネスク")
+        {
+            data.parts[ARMAMENT] = 0;
+            data.parts[VARIANT] = 0;
+            data.parts[ALTER] = 2;
+         
+        }
+        else if (input_text.text == "サイケデリック")
+        {
+            data.parts[ARMAMENT] = 0;
+            data.parts[VARIANT] = 0;
+            data.parts[ALTER] = 1;
+           
+        }
+        else
+        {
+            data.parts[ARMAMENT] = 0;
+            data.parts[VARIANT] = 0;
+            data.parts[ALTER] = 0;
+        }
+
+        data.name = input_text.text;
+        return data;
+    }
+
+   
+
+}
+
---
+++ Nechronica/Assets/tatumi/Script/Input_data_Button/WeponData_Set.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WeponData_Set : CharaBase
+{
+    [SerializeField]
+    private int EffectNum,Cost,Timing,MinRange,MaxRange,Weight, AtkType, Num_per_Action;//基礎設定値
+
+    [SerializeField]
+    private Text Name;//パーツ名
+
+    [SerializeField]
+    private bool isExplosion, isCotting, isAllAttack, isSuccession;//使用したかどうか
+
+    public CharaManeuver Set_Parts = new CharaManeuver { };
+    public ManeuverEffectsAtk Set_Eff = new ManeuverEffectsAtk { };
+
+    public Wepon_Maneger Wepon_maneger;
+    public int Type,level;
+
+
+    private void Start()
+    {
+        Set_Parts.Cost = Cost;
+        Set_Parts.EffectNum = EffectNum;
+        Set_Parts.isDmage = false;
+        Set_Parts.isUse = false;
+        Set_Parts.MaxRange = MaxRange;
+        Set_Parts.MinRange = MinRange;
+        Set_Parts.Name = Name.text;
+        Set_Parts.Timing = Timing;
+        Set_Parts.Weight = Weight;
+
+        Set_Eff.AtkType = AtkType;
+        Set_Eff.isAllAttack = isAllAttack;
+        Set_Eff.isCotting = isCotting;
+        Set_Eff.isExplosion = isExplosion;
+        Set_Eff.isSuccession = isSuccession;
+        Set_Eff.Num_per_Action = Num_per_Action;
+
+    }
+
+    //引数2個出来んためこっちで制御
+    public void Wepon_add()
+    {
+        Wepon_maneger.Add_Wepon(this.GetComponent<Toggle>(), Type,level);
+    }
+
+    public string Get_Wepon_Text()
+    {
+        return Name.text;
+    }
+}
