71e54d28a4f47a4113c14bf16902baad062ba96d
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2022-12-09 17:09:11 +0900

    らぴくん判定

--- Nechronica/Assets/tatumi/Script/test_chara/EnemyData/ObjEnemy.cs
+++ Nechronica/Assets/tatumi/Script/test_chara/EnemyData/ObjEnemy.cs
@@ -157,7 +157,7 @@ public class ObjEnemy : ClassData_
                 if (!Maneuvers[(int)EnemyPartsType.EAction][ActManeuvers].isDmage)
                 {
                     //敵の位置検索 & 射程比較
-                    if (Maneuvers[(int)EnemyPartsType.EAction][ActManeuvers].MinRange != 10 &&                                   //ここarea考えろ
+                    if (Maneuvers[(int)EnemyPartsType.EAction][ActManeuvers].MinRange != 10 &&                                  
                     (Mathf.Abs(PlayerDolls[i].area) <= Mathf.Abs(Maneuvers[(int)EnemyPartsType.EAction][ActManeuvers].MaxRange + me.area) &&
                      Mathf.Abs(PlayerDolls[i].area) >= Mathf.Abs(Maneuvers[(int)EnemyPartsType.EAction][ActManeuvers].MinRange + me.area)))
                     {
@@ -179,40 +179,67 @@ public class ObjEnemy : ClassData_
         return UseManever;
     }
 
-    public CharaManeuver EnemyAI_NonAction(EnemyPartsType PartsType)
+    public CharaManeuver EnemyAI_Rapid(CharaManeuver OpponentManeuver, Doll_blu_Nor Opponent)
     {
-        //全員とる
+        //味方のみとる
         List<Doll_blu_Nor> PlayerDolls = ManagerAccessor.Instance.battleSystem.GetCharaObj();
+
+        int MaxOpponentRange = OpponentManeuver.MaxRange;
+        int MinopponentRange = OpponentManeuver.MinRange;
 
         // 敵キャラのエリアと選択されたマニューバの射程を絶対値で比べて、射程内であれば攻撃するか選択するコマンドを表示する
         // 敵キャラのエリアの絶対値が攻撃の最大射程以下且つ、
         // 敵キャラのエリアの絶対値が攻撃の最小射程以上なら攻撃する
-
         //使用武具for文
-        for (int ActManeuvers = 0; ActManeuvers != Maneuvers[(int)PartsType].Count; ActManeuvers++)
+        for (int ActManeuvers = 0; ActManeuvers != Maneuvers[(int)EnemyPartsType.ERapid].Count; ActManeuvers++)
         {
-            for (int i = 0; i != PlayerDolls.Count; i++)
+            //破損判定&使用判定
+            if (!Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].isDmage && !Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].isDmage)
             {
-                //破損判定
-                if (!Maneuvers[(int)PartsType][ActManeuvers].isDmage)
+                //敵の位置検索 & 射程比較
+                if (Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].MinRange != 10 &&
+                (Mathf.Abs(Opponent.area) <= Mathf.Abs(Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].MaxRange + me.area) &&
+                 Mathf.Abs(Opponent.area) >= Mathf.Abs(Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].MinRange + me.area)))
                 {
-                    //敵の位置検索 & 射程比較
-                    if (Maneuvers[(int)PartsType][ActManeuvers].MinRange != 10 &&                                   
-                    (Mathf.Abs(PlayerDolls[i].area) <= Mathf.Abs(Maneuvers[(int)PartsType][ActManeuvers].MaxRange + me.area) &&
-                     Mathf.Abs(PlayerDolls[i].area) >= Mathf.Abs(Maneuvers[(int)PartsType][ActManeuvers].MinRange + me.area)))
+
+                    //相手の武具から射程にどっち方向かつどれだけ余裕があるか
+                    //例:狙われている武具射程が2~0で今の状況が射程の数字でいうところの2,1,0のどれかを算出
+                    int differenceRange = Mathf.Abs(Opponent.area - me.area) - MaxOpponentRange;
+                    if (Mathf.Abs(differenceRange) > Mathf.Abs(Mathf.Abs(Opponent.area - me.area) - MinopponentRange))
+                        differenceRange = Mathf.Abs(Opponent.area - me.area) - MinopponentRange;
+
+                    //この時点で数字=射程差,+=天国側,-地獄側が分かる,0の場合は別途
+                    //今検証中のPartsが射程外になるまで効果が及ぼせるか判定
+                    if (Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].Moving > Mathf.Abs(differenceRange))
                     {
+                        //効果が及ぼせるとき
                         //使用武具更新
                         if (UseManever == null)
                         {
-                            UseManever = Maneuvers[(int)PartsType][ActManeuvers];
+                            UseManever = Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers];
                             //優先度更新
                             //UseManever.EnemyAI[(int)EnemyPartsType.EAction] = 0;
                         }
-                        else if (UseManever.EnemyAI[(int)PartsType] < Maneuvers[(int)PartsType][ActManeuvers].EnemyAI[(int)PartsType])
-                            UseManever = Maneuvers[(int)PartsType][ActManeuvers];
+                        else if (UseManever.EnemyAI[(int)EnemyPartsType.ERapid] < Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers].EnemyAI[(int)EnemyPartsType.ERapid])
+                            UseManever = Maneuvers[(int)EnemyPartsType.ERapid][ActManeuvers];
+
+                        //天国側へ移動
+                        if (differenceRange > 0)
+                            ;
+                        //地獄側へ移動
+                        else
+                            ;
                     }
+
+                    
+                }
+                //自身に及ぼす場合
+                else
+                {
+
                 }
             }
+
         }
 
         //田中さんが書いた処理なので田中さんに聞いてください
