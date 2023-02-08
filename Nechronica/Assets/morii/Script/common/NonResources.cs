3c653cda044333681883af5bbe9b9ca504ad1abd
TatumiAkimasa <72424966+TatumiAkimasa@users.noreply.github.com>
2023-02-08 16:47:21 +0900

    Efctやったぜ

--- Nechronica/Assets/morii/Script/common/NonResources.cs
+++ Nechronica/Assets/morii/Script/common/NonResources.cs
@@ -16,7 +16,7 @@ public static class NonResources
     /// </summary>
     public static T Load<T>(string path) where T : Object
     {
-        return Load<T>(path);
+        return Resources.Load<T>(path);
     }
 
     /// <summary>
